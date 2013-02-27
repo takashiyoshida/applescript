2b8c4209db463006befcd30a6b874eff20c2fcce
Takashi Yoshida <takashi@takashiyoshida.org>
2013-02-28 00:21:07 +0800

    Update install.sh to compile output to ~/Library/Scripts directory

--- install.sh
+++ install.sh
@@ -7,5 +7,13 @@ Compile()
 	osacompile -o "${OUTPUT}" ${SOURCE}
 }
 
-Compile "Sync with iOS Devices.scpt" "sync_with_ios_devices.applescript"
-Compile "Download Podcasts.scpt" "download_podcasts.applescript"
+USER_SCRIPT="${HOME}/Library/Scripts"
+
+# For some of these scripts (for now, all), the compiled form should go to
+# ~/Library/
+
+# To install under ~/Library/ so that I can use them from any applications
+Compile "${USER_SCRIPT}/Sync with iOS Devices.scpt" "sync_with_ios_devices.applescript"
+Compile "${USER_SCRIPT}/Download Podcasts.scpt" "download_podcasts.applescript"
+
+Compile "${USER_SCRIPT}/Switch to Quiet Mode.scpt" "switch_to_quiet_mode.applescript"
